e5cf401a2ee6af78cb8a59ce1a09551836526c0a
jihyeon-e <jhw02040@gmail.com>
2021-08-05 02:38:48 +0900

    ex3-jpa-shop 프로젝트 생성

---
+++ ex3-jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        //엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+        //트랜잭션 기능 획득
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); //트랜잭션 시작
+
+        try {
+            Order order = new Order();
+            //order.addOrderItem(new OrderItem());
+
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+            tx.commit();//트랜잭션 커밋
+        } catch (Exception e) {
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+
+        emf.close(); //엔티티 매니저 팩토리 종료
+    }
+}
