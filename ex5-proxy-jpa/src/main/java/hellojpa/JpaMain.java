7b8fbcbd1bef82c610788261df5468615ba31c45
jihyeon-e <jhw02040@gmail.com>
2021-08-06 01:37:10 +0900

    프록시 예제 코드

--- ex5-proxy-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex5-proxy-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -18,21 +20,24 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
+            member.setUsername("member");
             em.persist(member);
 
             em.flush();
             em.clear();
 
+            Member reference = em.getReference(Member.class, member.getId());
+            System.out.println("reference = " + reference.getClass()); //Proxy
+            Hibernate.initialize(reference); //강제초기화
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            System.out.println("e = "+ e);
         } finally {
             em.close();
         }
         emf.close();
     }
+
 }
---
+++ ex5-proxy-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,25 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Member extends BaseEntity{
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
+}
